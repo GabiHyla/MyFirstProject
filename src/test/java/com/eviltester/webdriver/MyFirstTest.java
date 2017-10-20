3d48c547db81c291e38a956119c00d0f124b0a4f
Gabriela Hyla <gabi.hyla@gmail.com>
2017-10-21 00:51:34 +0100

    changes after code review

--- src/test/java/com/eviltester/webdriver/MyFirstTest.java
+++ src/test/java/com/eviltester/webdriver/MyFirstTest.java
@@ -4,18 +4,15 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.safari.SafariDriver;
 
 public class MyFirstTest {
+
     @Test
     public void startWebDriver() {
         WebDriver driver = new ChromeDriver();
-
         driver.navigate().to("https://seleniumsimplified.com");
-
         Assert.assertTrue("title should start differently",
                             driver.getTitle().startsWith("Selenium Simplified"));
-
         driver.close();
 
     }
