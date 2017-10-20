88ba56554cc0aff7f715c21e19c6718a5e916d29
Gabriela Hyla <gabi.hyla@gmail.com>
2017-10-21 00:19:20 +0100

    My First Selenium Test

---
+++ src/test/java/com/eviltester/webdriver/MyFirstTest.java
@@ -0,0 +1,22 @@
+package com.eviltester.webdriver;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.safari.SafariDriver;
+
+public class MyFirstTest {
+    @Test
+    public void startWebDriver() {
+        WebDriver driver = new ChromeDriver();
+
+        driver.navigate().to("https://seleniumsimplified.com");
+
+        Assert.assertTrue("title should start differently",
+                            driver.getTitle().startsWith("Selenium Simplified"));
+
+        driver.close();
+
+    }
+}
